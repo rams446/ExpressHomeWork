a666e275888da178023c691702eb8232b0425d90
Ramya Sree <ramyasree@Ramyas-Laptop.local>
2023-04-28 00:00:56 -0500

    new commit

--- server.js
+++ server.js
@@ -4,17 +4,23 @@ const app = express();
 const PORT=process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send(`<h1>99 BEERS </h1>
-  <a href=${'/98'} > take one down </a>  `)
+  res.send(`<h3>99 Beers Left</h3>
+  <br/>
+  <a href=${'/98'}>take one down, pass it around</a>  `)
 });
 
-app.get('/98', (req, res) => {
-    res.send(`<h1>98</h1>`)
-  });
+// app.get('/98', (req, res) => {
+//     res.send(`<h1>98</h1>`)
+//   });
 
 app.get('/:number_of_bottles', (req, res) => {
- 
-        res.send(`${req.params.number_of_bottles} <a href=${req.params.number_of_bottles-1} > take one down </a>`);
+        let number = req.params.number_of_bottles;
+        console.log(number);
+        if(number == 0){
+            res.send('<a href="/"> Start over </a>');            
+        }else{
+            res.send(`<h3>${req.params.number_of_bottles} Beers Left</h3> <br/><a href=${number-1}>take one down, pass it around</a>`);
+        }
 });
 
 
